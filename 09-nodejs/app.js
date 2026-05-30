797da7b59b32492b035f4be4b667a051c4fc1918
MantashaNoyela <mantashanoyela4@gmail.com>
2026-05-29 23:58:12 -0700

    Nodejs Exercise

--- 09-nodejs/app.js
+++ 09-nodejs/app.js
@@ -12,20 +12,23 @@ const server = http.createServer((req, res) => {
     file.serveFile('/welcome.html', 200, {}, req, res);
   }
   // form route
-  else if (req.method === 'GET' && req.url === '') {
+  else if (req.method === 'GET' && req.url === '/form') {
     // fill out this route
+    file.serveFile('/form.html', 200, {}, req, res);
   }
   // form submission
-  else if (req.method === 'POST' && req.url === '') {
+  else if (req.method === 'POST' && req.url === '/formExerciseSubmit') {
     let body = '';
 
     req.on('data', (chunk) => {
       body += chunk;
+      console.log(body);
     });
 
     req.on('end', () => {
       const userdata = querystring.parse(body);
-      const { usernameInput: name, emailInput: email } = userdata;
+      console.log(userdata);
+      const { nameInput: name, emailInput: email } = userdata;
 
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(`<p>Thank you for submitting your information: </p>`);
